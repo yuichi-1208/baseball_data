5ebbdc98bda5d110da78a34809337a3c4ff9b9df
yuichi oyama <yuichi19991208@gmail.com>
2020-10-23 16:06:15 +0900

    テスト駆動開発の実践2

--- app/models/pitcher.rb
+++ app/models/pitcher.rb
@@ -6,4 +6,16 @@ class Pitcher < ApplicationRecord
     best_pitchers.pluck(:player_id)
   end
 
+  def self.top_ten(year)
+    pitchers = Pitcher.where(year_id: year).order(wins: :desc)
+    if pitchers.count < 10
+      best_pitchers = pitchers
+    else
+      tenth_win = pitchers.limit(10)[9].wins
+      best_pitchers = pitchers.where("wins >= ?", tenth_win)
+    end
+    best_pitchers.pluck(:player_id)
+  end
+
+
 end
--- spec/models/pitcher_spec.rb
+++ spec/models/pitcher_spec.rb
@@ -18,4 +18,51 @@ describe Pitcher do
     Pitcher.create(player_id: 'siro', year_id: 2001, wins: 10)
     expect(Pitcher.most_winners(2000)).to eq []
   end
+
+  it "top_tenメソッド使用時、上から10番目の投手と同じ勝利数の投手がいない場合" do
+    Pitcher.create(player_id: 'taro', year_id: 2000, wins: 15)
+    Pitcher.create(player_id: 'jiro', year_id: 2000, wins: 14)
+    Pitcher.create(player_id: 'saburo', year_id: 2000, wins: 13)
+    Pitcher.create(player_id: 'siro', year_id: 2000, wins: 12)
+    Pitcher.create(player_id: 'goro', year_id: 2000, wins: 11)
+    Pitcher.create(player_id: 'rokuro', year_id: 2000, wins: 20)
+    Pitcher.create(player_id: 'sitiro', year_id: 2000, wins: 19)
+    Pitcher.create(player_id: 'hatiro', year_id: 2000, wins: 18)
+    Pitcher.create(player_id: 'kuro', year_id: 2000, wins: 17)
+    Pitcher.create(player_id: 'juro', year_id: 2000, wins: 16)
+    Pitcher.create(player_id: 'juichiro', year_id: 2000, wins: 10)
+    Pitcher.create(player_id: 'jujiro', year_id: 2001, wins: 18)
+    expect(Pitcher.top_ten(2000)).to eq ["rokuro","sitiro","hatiro","kuro","juro","taro","jiro","saburo","siro","goro"]
+  end
+  it "top_tenメソッド使用時、上から10番目の投手と同じ勝利数の投手がいる場合" do
+    Pitcher.create(player_id: 'taro', year_id: 2000, wins: 15)
+    Pitcher.create(player_id: 'jiro', year_id: 2000, wins: 14)
+    Pitcher.create(player_id: 'saburo', year_id: 2000, wins: 13)
+    Pitcher.create(player_id: 'siro', year_id: 2000, wins: 12)
+    Pitcher.create(player_id: 'goro', year_id: 2000, wins: 11)
+    Pitcher.create(player_id: 'rokuro', year_id: 2000, wins: 20)
+    Pitcher.create(player_id: 'sitiro', year_id: 2000, wins: 19)
+    Pitcher.create(player_id: 'hatiro', year_id: 2000, wins: 18)
+    Pitcher.create(player_id: 'kuro', year_id: 2000, wins: 17)
+    Pitcher.create(player_id: 'juro', year_id: 2000, wins: 17)
+    Pitcher.create(player_id: 'juichiro', year_id: 2000, wins: 11)
+    Pitcher.create(player_id: 'jujiro', year_id: 2000, wins: 10)
+    Pitcher.create(player_id: 'jujiro', year_id: 2001, wins: 18)
+    expect(Pitcher.top_ten(2000)).to eq ["rokuro","sitiro","hatiro","juro","kuro","taro","jiro","saburo","siro","goro","juichiro"]
+  end
+  it "top_tenメソッド使用時、指定した年のデータが10件に満たない場合" do
+    Pitcher.create(player_id: 'taro', year_id: 2000, wins: 15)
+    Pitcher.create(player_id: 'jiro', year_id: 2000, wins: 14)
+    Pitcher.create(player_id: 'hatiro', year_id: 2000, wins: 18)
+    Pitcher.create(player_id: 'kuro', year_id: 2000, wins: 17)
+    Pitcher.create(player_id: 'juro', year_id: 2000, wins: 17)
+    Pitcher.create(player_id: 'juichiro', year_id: 2000, wins: 11)
+    Pitcher.create(player_id: 'jujiro', year_id: 2000, wins: 10)
+    Pitcher.create(player_id: 'jusaburo', year_id: 2001, wins: 18)
+    expect(Pitcher.top_ten(2000)).to eq ["hatiro","kuro","juro","taro","jiro","juichiro","jujiro"]
+  end
+  it "top_tenメソッド使用時、指定した年のデータが存在しない場合" do
+    Pitcher.create(player_id: 'jusaburo', year_id: 2001, wins: 18)
+    expect(Pitcher.top_ten(2000)).to eq []
+  end
 end
